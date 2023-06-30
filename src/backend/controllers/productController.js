b9bdd6c3e95c19f3d1d3943293bff06f82ce353c
Tarun Singh <tarunsingh22057@gmail.com>
2023-06-30 16:33:32 +0530

    ADDED : error handling to product controller

--- src/backend/controllers/productController.js
+++ src/backend/controllers/productController.js
@@ -2,39 +2,79 @@ const Product = require("../models/productModel");
 
 // get all products
 const getAllProducts = async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json({ success: true, products });
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 };
 
 // get products by gender
 const getProductsByGender = async (req, res) => {
-  const gender = req.params.gender.toUpperCase();
-  const productsByGender = await Product.find({ gender });
-  res.status(200).json({ success: true, productsByGender });
+  try {
+    const gender = req.params.gender.toUpperCase();
+    const productsByGender = await Product.find({ gender });
+
+    if (productsByGender.length === 0) {
+      res.status(404).json({ success: false, error: "No products found" });
+    } else {
+      res.status(200).json({ success: true, productsByGender });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 };
 
 // get products by brand
 const getProductsByBrand = async (req, res) => {
-  const brand = req.params.brand;
-  const productsByBrand = await Product.find({ brand });
-  res.status(200).json({ success: true, productsByBrand });
+  try {
+    const brand = req.params.brand.toUpperCase();
+    const productsByBrand = await Product.find({ brand });
+
+    if (productsByBrand.length === 0) {
+      res.status(404).json({ success: false, error: "No products found" });
+    } else {
+      res.status(200).json({ success: true, productsByBrand });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 };
 
 // get products by category
 const getProductsByCategory = async (req, res) => {
-  const category = req.params.category;
-  const productsByCategory = await Product.find({ category });
-  res.status(200).json({ success: true, productsByCategory });
+  try {
+    const category = req.params.category.toUpperCase();
+    const productsByCategory = await Product.find({ category });
+
+    if (productsByCategory.length === 0) {
+      res.status(404).json({ success: false, error: "No products found" });
+    } else {
+      res.status(200).json({ success: true, productsByCategory });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 };
 
 // get products by price
 const getProductsByPrice = async (req, res) => {
-  const minPrice = parseFloat(req.query.minPrice) || 0;
-  const maxPrice = parseFloat(req.query.maxPrice) || Infinity;
-  const productsByPrice = await Product.find({
-    price: { $gte: minPrice, $lte: maxPrice },
-  });
-  res.status(200).json({ success: true, productsByPrice });
+  try {
+    const minPrice = parseFloat(req.query.minPrice) || 0;
+    const maxPrice = parseFloat(req.query.maxPrice) || Infinity;
+    const productsByPrice = await Product.find({
+      price: { $gte: minPrice, $lte: maxPrice },
+    });
+
+    if (productsByPrice.length === 0) {
+      res.status(404).json({ success: false, error: "No products found" });
+    } else {
+      res.status(200).json({ success: true, productsByPrice });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 };
 
 module.exports = {
